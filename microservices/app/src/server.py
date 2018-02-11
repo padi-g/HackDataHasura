a4e3ce59e53bb31281db18426e4060dc9efbcb6a
Gautam Padiyar <gautam.padiyar@gmail.com>
2018-02-11 11:21:04 +0530

    print

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -31,6 +31,54 @@ def load_model():
     # load weights into new model
     loaded_model3.load_weights("src/model3.h5")
 
+def categorize(data):    
+    if data.age < '15':
+        data.age = 1
+    elif data.age < '25':
+        data.age = 2
+    elif data.age < '35':
+        data.age = 3
+    elif data.age < '45':
+        data.age = 4
+    elif data.age < '55':
+        data.age = 5
+    elif data.age < '65':
+        data.age = 6
+    else:
+        data.age = 7
+
+    if data.sex == "male":
+        data.sex = 0
+    else:
+        data.sex = 1
+
+    if data.duration < '2':
+        data.duration = 1
+    elif data.duration < '3':
+        data.duration = 2
+    elif data.duration < '6':
+        data.duration = 3
+    elif data.duration < '10':
+        data.duration = 4
+    elif data.duration < '15':
+        data.duration = 5
+    else:
+        data.duration = 6
+
+    if data.budget < '50000':
+        data.budget = 1
+    elif data.budget < '200000':
+        data.budget = 2
+    elif data.budget < '500000':
+        data.budget = 3
+    elif data.budget < '2000000':
+        data.budget = 4
+    elif data.budget < '5000000':
+        data.budget = 5
+    else:
+        data.budget = 6
+
+
 
 @app.route("/")
 def home():
@@ -42,7 +90,7 @@ def json_message():
 
     rdata = flask.jsonify(flask.request.args.to_dict())
     print(flask.request)
-    print(rdata["age"])
+    print(rdata.age)
     # return flask.jsonify(flask.request.args.to_dict())
     # conversion
 
