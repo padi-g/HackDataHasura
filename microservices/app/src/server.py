1f0e493927594c8c630b883b3952da46427a738a
Gautam Padiyar <gautam.padiyar@gmail.com>
2018-02-11 06:27:28 +0530

    trying again

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -3,83 +3,79 @@ import numpy as np
 from keras.models import model_from_json
 from flask import jsonify
 
+load_model()
+
+
 def load_model():
+    global loaded_model1, loaded_model2, loaded_model3
+    json_file = open('model1.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model1 = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model1.load_weights("model1.h5")
 
-	global loaded_model1, loaded_model2, loaded_model3
-	json_file = open('model1.json', 'r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	loaded_model1 = model_from_json(loaded_model_json)
-	# load weights into new model
-	loaded_model1.load_weights("model1.h5")
+    json_file = open('model2.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model2 = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model2.load_weights("model2.h5")
 
-	json_file = open('model2.json', 'r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	loaded_model2 = model_from_json(loaded_model_json)
-	# load weights into new model
-	loaded_model2.load_weights("model2.h5")
-
-	json_file = open('model3.json', 'r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	loaded_model3 = model_from_json(loaded_model_json)
-	# load weights into new model
-	loaded_model3.load_weights("model3.h5")
+    json_file = open('model3.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model3 = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model3.load_weights("model3.h5")
 
 
 @app.route("/")
 def home():
     return "Hasura Hello World"
 
+
 @app.route("/predict", methods=["GET"])
 def json_message():
-
     rdata = flask.request.args.to_dict()
 
-	# conversion
+    # conversion
 
-	x_in = np.random.randn(1,5)
-	x_in[0][0] = 1
-	x_in[0][1] = 3
-	x_in[0][2] = 1	
-	x_in[0][3] = 4
-	x_in[0][4] = 5
+    x_in = np.random.randn(1, 5)
+    x_in[0][0] = 1
+    x_in[0][1] = 3
+    x_in[0][2] = 1
+    x_in[0][3] = 4
+    x_in[0][4] = 5
 
-	pred1 = loaded_model1.predict(x_in)
-	pred2 = loaded_model2.predict(x_in)
-	pred3 = loaded_model3.predict(x_in)
+    pred1 = loaded_model1.predict(x_in)
+    pred2 = loaded_model2.predict(x_in)
+    pred3 = loaded_model3.predict(x_in)
 
-	pred = (pred1 + pred2 + pred3)/3
+    pred = (pred1 + pred2 + pred3) / 3
 
-	i = iter(pred)
-	pred_dict = {pred[0][i]: i for i in range(0, 85)}
+    i = iter(pred)
+    pred_dict = {pred[0][i]: i for i in range(0, 85)}
 
-	pred_dict = sorted(pred_dict.items())
+    pred_dict = sorted(pred_dict.items())
 
-	prefs = []
+    prefs = []
 
-	for _,value in pred_dict:
-		prefs.append(value)
+    for _, value in pred_dict:
+        prefs.append(value)
 
-	prefs.reverse()
-	data = {}
+    prefs.reverse()
+    data = {}
 
-	with open('CountryDB.txt') as f:
-		cnt = 1
-		line = f.readline()
-		while line:
-			data[cnt] = line.strip()
-			line = f.readline()
-			cnt += 1
+    with open('CountryDB.txt') as f:
+        cnt = 1
+        line = f.readline()
+        while line:
+            data[cnt] = line.strip()
+            line = f.readline()
+            cnt += 1
 
-	for i in range(num_queries):
-		print(data[prefs[i]])
-			
-	return flask.jsonify(data)
+    for i in range(num_queries):
+        print(data[prefs[i]])
 
-if __name__ == "__main__":
-	print(("* Loading Keras model and Flask starting server..."
-		"please wait until server has fully started"))
-	load_model()
-	app.run()
+    return flask.jsonify(data)
