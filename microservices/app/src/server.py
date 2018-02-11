82f326ca48ff99fe3106aa4b87e1c2855538d94e
Gautam Padiyar <gautam.padiyar@gmail.com>
2018-02-11 08:39:35 +0530

    print

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -87,7 +87,7 @@ def home():
 @app.route("/predict", methods=["GET"])
 def json_message():
 
-    rdata = flask.request.args.to_dict()
+    print(flask.request.args.to_dict())
     categorize(rdata)
     # return flask.jsonify(flask.request.args.to_dict())
     # conversion
