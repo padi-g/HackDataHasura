6d0f7cbdc96f705e676c6b74795b1babd07369d3
Gautam Padiyar <gautam.padiyar@gmail.com>
2018-02-11 07:13:23 +0530

    added globals

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -8,6 +8,7 @@ import os
 
 def load_model():
 	print(os.getcwd())
+	global loaded_model1,loaded_model2,loaded_model3
 	json_file = open('src/model1.json', 'r')
 	loaded_model_json = json_file.read()
 	json_file.close()
